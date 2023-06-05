20dc57d4d419bfeba353f260d8d7467026fbafbf
Vismyfriend <vismyfriend@yahoo.com>
2023-06-05 20:42:54 +0300

    easy questions

---
+++ scripts/utils/ques-answ.js
@@ -0,0 +1,25 @@
+const questions = [
+    {					
+        id	:	1	,		
+        ru	:	"	1	"	,
+        eng	:	"	2	"	
+        }	,				
+        {					
+        id	:	2	,		
+        ru	:	"	2	"	,
+        eng	:	"	2	"	
+        }	,				
+        					
+]
+const answers = [
+    {
+        id: 101,
+        ru: "	Ты красавчик / красотка !",
+        eng: "	You are sexy! "
+    },
+    {
+        id: 102,
+        ru: "	What did you buy yesterday? What do you want to buy next month?",
+        eng: "	придумай вопрос с глаголом  buy "
+    },
+]	
